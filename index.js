ae48785c495fa24936afa0c750bc0e1fc1a0aa06
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-13 10:30:54 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,50 @@
 
 function whoRunsTheWorld()
 {
-    console.log("Bots!! Bots!!");
+    console.log("Who Runs The World?");
 }
 
 whoRunsTheWorld();
+
+function response()
+{
+    console.log("Bots!! Bots!!");
+}
+
+response();
+
+function whoRunsTheWorld2()
+{
+    console.log("Who Runs The World?");
+}
+
+whoRunsTheWorld2();
+
+function response2()
+{
+    console.log("Bots!! Bots!!");
+}
+
+response2();
+
+
+function whoRunsThisMadda()
+{
+    console.log("Who Runs This Madda?");
+}
+
+whoRunsThisMadda();
+
+
+function response3()
+{
+    console.log("Bots!! Bots!!");
+}
+
+response3();
+
+function justKidding()
+{console.log("DISCLAIMER: Bots actually do not run the world. We believe. At least, to the best of our knowledge");}
+justKidding();
+
+
